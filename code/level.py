ad849fde378f8d969f29858fdb180e3364a95c3a
Jung Lee <113197001+junglee0035@users.noreply.github.com>
2025-04-23 16:00:22 +0000

    Updated collisions

--- code/level.py
+++ code/level.py
@@ -8,12 +8,15 @@ class Level:
 
         #groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
+        #tiles
         for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
- 
+
+        #objects
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
                 Player((obj.x, obj.y), self.all_sprites)
