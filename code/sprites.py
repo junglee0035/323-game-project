96352dc2663a69548637c7df6cb423349a386139
pamelamensah <145529832+pamelamensah@users.noreply.github.com>
2025-04-24 12:49:26 -0500

    Update sprites.py

--- code/sprites.py
+++ code/sprites.py
@@ -5,3 +5,10 @@ class Sprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft = pos)
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, coin_image, value=1):
+        super().__init__(groups)
+        self.image = coin_image
+        self.rect = self.image.get_rect(topleft=pos)
+        self.value = value
