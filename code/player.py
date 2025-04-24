8b2fde53eb5663d49f45b1909fe9eabf7323f1ee
Jung Lee <113197001+junglee0035@users.noreply.github.com>
2025-04-24 17:34:34 +0000

    implemented sprite

--- code/player.py
+++ code/player.py
@@ -1,24 +1,23 @@
-import pygame
-from settings import *
+from settings import * 
+import os
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
-        self.image = pygame.Surface((32, 32))  # Placeholder for player sprite
-        self.image.fill('blue')  # Fill with a color for visibility
+        sprite_path = os.path.join("graphics", "player", "ball4En.png")
+        self.image = pygame.image.load(sprite_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (32, 32))
         self.rect = self.image.get_rect(topleft=pos)
 
         # Movement attributes
-        self.speed = 200  # Pixels per second
+        self.speed = 200
         self.direction = pygame.math.Vector2(0, 0)
-        input_vector = vector(0, 0)
+
         self.gravity = 1300
-        self.default_jump_force = -500
-        self.jump_force = self.default_jump_force
+        self.jump_force = -600
         self.jump = False
         self.jump_buffered = False
         self.on_ground = False
-        
 
     def handle_input(self):
         keys = pygame.key.get_pressed()
@@ -40,42 +39,44 @@ class Player(pygame.sprite.Sprite):
                 self.jump_buffered = True
             else:
                 self.jump_buffered = False
-    
+               
+        
     def apply_gravity(self, dt):
         self.direction.y += self.gravity * dt
         if self.direction.y > 1000:
             self.direction.y = 1000
 
     def move(self, dt, solids):
-        # Move horizontally and check for collisions
+        # Horizontal movement
         self.rect.x += self.direction.x * self.speed * dt
         for solid in solids:
             if self.rect.colliderect(solid.rect):
-                if self.direction.x > 0:  # Moving right
+                if self.direction.x > 0:
                     self.rect.right = solid.rect.left
-                elif self.direction.x < 0:  # Moving left
+                elif self.direction.x < 0:
                     self.rect.left = solid.rect.right
 
-        # Move vertically and check for collisions
-        self.rect.y += self.direction.y * dt
+        # Vertical movement
+        self.rect.y += self.direction.y * dt  # Use gravity-scaled direction.y directly
         self.on_ground = False
         for solid in solids:
             if self.rect.colliderect(solid.rect):
-                if self.direction.y > 0:  # Moving down
+                if self.direction.y > 0:
                     self.rect.bottom = solid.rect.top
                     self.direction.y = 0
                     self.on_ground = True
-                elif self.direction.y < 0:  # Moving up
+                elif self.direction.y < 0:
                     self.rect.top = solid.rect.bottom
                     self.direction.y = 0
+        
+    
 
     def update(self, dt, solids):
         self.handle_input()
 
-        # prevents double jumping
         if self.jump and self.on_ground:
             self.direction.y = self.jump_force
             self.jump = False
-            
+
         self.apply_gravity(dt)
         self.move(dt, solids)
